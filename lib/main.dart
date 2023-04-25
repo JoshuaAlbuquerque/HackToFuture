861d17fc46da36674fd1862620f62d33ba88b11a
JoshuaAlbuquerque <22f43.joshua@sjec.ac.in>
2023-04-25 14:03:57 +0530

    added floor pages

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@
   import 'package:flutter_grid_button/flutter_grid_button.dart';
   import 'package:css_colors/css_colors.dart';
   import 'profile.dart';
-  import 'house2.dart';
+  import 'house2-floor2.dart';
   import 'house4.dart';
   
   void main() {
