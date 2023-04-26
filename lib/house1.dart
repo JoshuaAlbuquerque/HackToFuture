164e996436f53d8edf88c1492e74e0fe1f1f2b67
JoshuaAlbuquerque <22f43.joshua@sjec.ac.in>
2023-04-26 06:03:12 +0530

    changed the layout for house 1

---
+++ lib/house1.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+final random = Random();
+final num = random.nextInt(9) + 13;
+final time = random.nextInt(5) + 20;
+
+class House1 extends StatelessWidget {
+  const House1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'Tokyo House',
+            style: TextStyle(
+              fontSize: 25.0,
+              color: Colors.white,
+              fontFamily: 'Cabin',
+            ),
+          ),
+          backgroundColor: const Color(0xFF3C096C),
+        ),
+        body: InteractiveViewer(
+          boundaryMargin: const EdgeInsets.all(0),
+          constrained: false,
+          scaleEnabled: false,
+          child: Image.asset("assets/venue.png"),
+          ),
+         ),
+    );
+  }
+}
